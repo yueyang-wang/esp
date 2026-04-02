60a93c6b6378dc1ef8d874891a730bc23df95c65
yueyang-wang <wangyueyang0220@126.com>
2026-04-02 10:20:15 +0800

    freertos/thread: reduce default stack size to 8KB

--- lib/component/freertos/Thread.zig
+++ lib/component/freertos/Thread.zig
@@ -17,7 +17,7 @@ const max_u64: u64 = ~@as(u64, 0);
 
 pub const Id = usize;
 pub const max_name_len: usize = 15;
-pub const default_stack_size: usize = 16384;
+pub const default_stack_size: usize = 8192;
 
 shared: *Shared,
 
