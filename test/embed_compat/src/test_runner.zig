29d38c7602b1226c11cd10a75bf2da44ad9643af
yueyang-wang <wangyueyang0220@126.com>
2026-04-02 10:22:40 +0800

    test/embed_compat: set runner stack size on esp

--- test/embed_compat/src/test_runner.zig
+++ test/embed_compat/src/test_runner.zig
@@ -5,6 +5,14 @@ const stb_truetype = @import("stb_truetype");
 const integration = @import("integration");
 const testing = @import("testing");
 
+const esp_stack_size: usize = 16384;
+
+fn withEspStack(tr: testing.TestRunner) testing.TestRunner {
+    var r = tr;
+    r.spawn_config = .{ .stack_size = esp_stack_size };
+    return r;
+}
+
 pub fn run(comptime lib: type) !void {
     const rtstd = lib.std;
     const Net = lib.net.make(rtstd);
@@ -21,27 +29,27 @@ pub fn run(comptime lib: type) !void {
     runner.parallel();
     runner.timeout(240 * rtstd.time.ns_per_s);
 
-    runner.run("sync/channel", lib.sync.test_runner.channel.make(rtstd, lib.Channel));
-    runner.run("sync/racer", lib.sync.test_runner.racer.make(rtstd));
-    runner.run("integration", integration.make(rtstd));
-    runner.run("net/fd_stream", lib.net.test_runner.fd_stream.make(rtstd));
-    runner.run("net/fd_packet", lib.net.test_runner.fd_packet.make(rtstd));
-    runner.run("net/tcp", lib.net.test_runner.tcp.make(rtstd));
-    runner.run("net/udp", lib.net.test_runner.udp.make(rtstd));
-    runner.run("net/resolver", lib.net.test_runner.resolver.make(rtstd));
-    runner.run("net/resolver_dns", lib.net.test_runner.resolver_dns.make(rtstd, &.{
-        Net.Resolver.dns.ali.v4_1,
-        Net.Resolver.dns.ali.v4_2,
-    }, Net.Resolver.dns.ali.server_name));
-    runner.run("net/tls", lib.net.test_runner.tls.make(rtstd));
-    runner.run("net/tls_dial", lib.net.test_runner.tls_dial.make(rtstd, Net.Resolver.dns.ali.server_name));
-    runner.run("net/ntp", lib.net.test_runner.ntp.make(rtstd));
-    runner.run("net/http_transport", lib.net.test_runner.http_transport.make(rtstd));
-    runner.run("net/https_transport", lib.net.test_runner.https_transport.make(rtstd));
-    runner.run("lvgl", lvgl.test_runner.lvgl.make(rtstd));
-    runner.run("stb_truetype", stb_truetype.test_runner.stb_truetype.make(rtstd));
-    runner.run("ogg", ogg.test_runner.ogg.make(rtstd));
-    runner.run("opus", opus.test_runner.opus.make(rtstd));
+    runner.run("sync/channel", withEspStack(lib.sync.test_runner.channel.make(rtstd, lib.Channel)));
+    runner.run("sync/racer", withEspStack(lib.sync.test_runner.racer.make(rtstd)));
+    runner.run("integration", withEspStack(integration.make(rtstd)));
+    runner.run("net/fd_stream", withEspStack(lib.net.test_runner.fd_stream.make(rtstd)));
+    runner.run("net/fd_packet", withEspStack(lib.net.test_runner.fd_packet.make(rtstd)));
+    runner.run("net/tcp", withEspStack(lib.net.test_runner.tcp.make(rtstd)));
+    runner.run("net/udp", withEspStack(lib.net.test_runner.udp.make(rtstd)));
+    runner.run("net/resolver", withEspStack(lib.net.test_runner.resolver.make(rtstd)));
+    runner.run("net/resolver_dns", withEspStack(lib.net.test_runner.resolver_dns.make(rtstd, &.{
+       Net.Resolver.dns.ali.v4_1,
+       Net.Resolver.dns.ali.v4_2,
+    }, Net.Resolver.dns.ali.server_name)));
+    runner.run("net/tls", withEspStack(lib.net.test_runner.tls.make(rtstd)));
+    runner.run("net/tls_dial", withEspStack(lib.net.test_runner.tls_dial.make(rtstd, Net.Resolver.dns.ali.server_name)));
+    runner.run("net/ntp", withEspStack(lib.net.test_runner.ntp.make(rtstd)));
+    runner.run("net/http_transport", withEspStack(lib.net.test_runner.http_transport.make(rtstd)));
+    runner.run("net/https_transport", withEspStack(lib.net.test_runner.https_transport.make(rtstd)));
+    runner.run("lvgl", withEspStack(lvgl.test_runner.lvgl.make(rtstd)));
+    runner.run("stb_truetype", withEspStack(stb_truetype.test_runner.stb_truetype.make(rtstd)));
+    runner.run("ogg", withEspStack(ogg.test_runner.ogg.make(rtstd)));
+    runner.run("opus", withEspStack(opus.test_runner.opus.make(rtstd)));
 
     const passed = runner.wait();
     app_log.info("embed-zig test runners finished", .{});
